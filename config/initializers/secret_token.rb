3e13345892f148e4427abb045afee1c8c22cc8f3
Jon Strand <jon.g.strand@gmail.com>
2013-01-19 15:12:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drj::Application.config.secret_token = '6fdc9e36ee80e5a923f793655531770eadc1b894dfd33ce84e8795c08f2b4bf06ea46027543b276f5d96a29118b9374c4b0348b6f70fdf02b764c1d213670528'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Drj::Application.config.session_store :cookie_store, key: '_drj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Drj::Application.config.session_store :active_record_store
