0ab4849d16b741a7ca7a55d9de51cac19cf4da90
Jon Strand <jon.g.strand@gmail.com>
2013-02-18 22:36:35 -0600

    prevent injection (precompile)

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,16 +1,20 @@
 class WordsController < ApplicationController
   require 'active_support/inflector'
   def single_word
-  	@results = Word.find_by_orth(params[:orth])
+    @results = Word.where("lower(orth) =?", params[:orth].downcase).first
+    if @results == nil
+      render "not_found"
+    end
   end
 
   def multiple_word
   	if params[:wordstring] then
-  		@wordarray = params[:wordstring]
+  		@wordarray = params[:wordstring].downcase.gsub(/\W|\d/) {" "}
   	else
   		@wordarray = "" 
   	end
   end
+
 end
 
 
