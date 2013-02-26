802229ba90e7981b46ef3bf8039edce01d58e5df
Jon Strand <jon.g.strand@gmail.com>
2013-02-25 22:25:05 -0600

    refactoring model and error handling

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,16 +3,28 @@ class WordsController < ApplicationController
   def single_word
     @results = Word.where("lower(orth) =?", params[:orth].downcase).first
     if @results == nil
+      @misses = params[:orth]
       render "not_found"
     end
   end
 
   def multiple_word
   	if params[:wordstring] then
-  		@wordarray = params[:wordstring].downcase.gsub(/\W|\d/) {" "}
+  		wordlist = params[:wordstring].downcase.gsub(/(,\s*|\s+)/) {" "}
   	else
-  		@wordarray = "" 
+  		wordlist = "" 
   	end
+    @misses = []
+    @words = []
+    wordlist.split(" ").each do |word|
+      wordmatch = Word.find(:first, :conditions => [ "lower(orth) = ?", word ]) #case insensitive 
+      if wordmatch.nil?
+        @misses << word
+      else
+        @words << wordmatch
+      end
+    end
+
   end
 
 end
